e2586397df21534a96ea95ec9a4b5d8a1f15f57d
freakyclown <freakyclown>
2014-02-27 09:52:26 +0000

    alpha

---
+++ 1curl2sqlmap.py
@@ -0,0 +1,52 @@
+import re
+import sys
+
+kommand = "~/pcsl/sqlmapproject-sqlmap-98d27ef/sqlmap.py -o -a --batch -t c2s.txt --proxy='http://127.0.0.1:8080' "
+
+header = ""
+cookie = ""
+data = ""
+url = ""
+
+
+def welcomescreen():
+	print "welcome"
+
+def curl_split(usercommand):
+	global cookie
+	global header
+	global data
+	global url
+	print "processing curl command"
+	patternsplit = re.compile(r'\\')
+	patternfindH = re.compile(r' -H \'(.+?)\'')
+	patternfindb = re.compile(r' -b \'(.+?)\'')
+	patternfinddata = re.compile(r' --data-binary \$\'(.+?)\'')
+	patternURL = re.compile(r'\'(.+?)\'$')
+
+	for split in re.split(patternsplit,usercommand):
+		for findH in re.findall(patternfindH,split):
+			if header == "":
+				header = findH
+			else:
+				header = header+"\\n"+findH
+		
+	        for findb in re.findall(patternfindb,split):
+			cookie = findb
+		for finddata in re.findall(patternfinddata,split):
+			data = finddata
+		for findURL in re.findall(patternURL,split):
+			 url = findURL
+	
+	print kommand, "-u "+url, "--cookie='"+cookie+"'", "--data='"+data+"'", "--headers=\""+header+"\""
+
+def paste_curlcommand():
+	print "input your curl command here"
+	#grab user input and fling to the next function
+	print "paste here and then press ctrl+d twice"
+	foob = sys.stdin.read()
+	curl_split(foob)
+
+
+welcomescreen()
+paste_curlcommand()
